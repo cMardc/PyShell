830847afbe376dbaaf27fd287d807b1a3d188046
cMardc <merd.yedek@gmail.com>
2023-09-09 11:46:06 +0400

    Configuration files

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from colorama import Fore, Style
 from pathlib import Path
 import tempfile
 from subprocess import call
+import json
 
 
 def view(directory):
@@ -45,21 +46,129 @@ def remove_space(text):
 
 
 def main():
+    json_file_path = "../config/color.json"
+    json_file_path2 = "../config/config.json"
+
+    color_dict = {}
+    config_dict = {}
+
+    try:
+        with open(json_file_path, "r") as json_file:
+            color_dict = json.load(json_file)
+    except FileNotFoundError:
+        print(f"JSON file not found at {json_file_path}")
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON file: {e}")
+
+    try:
+        with open(json_file_path2, "r") as json_file:
+            config_dict = json.load(json_file)
+    except FileNotFoundError:
+        print(f"JSON file not found at {json_file_path2}")
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON file: {e}")
+
     show_dir = False
-    print(Fore.MAGENTA + Startup.startup_text)
-    print(Fore.RESET)
+
+    if config_dict:
+        if config_dict["show_dir"]:
+            show_dir = config_dict["show_dir"]
+
+    if not color_dict:
+        print(Fore.MAGENTA + Startup.startup_text)
+        print(Fore.RESET)
+    else:
+        if color_dict["STARTUP"]:
+            current_color = color_dict["STARTUP"]
+            if current_color == "RED":
+                print(Fore.RED + Startup.startup_text)
+                print(Fore.RESET)
+            elif current_color == "BLUE":
+                print(Fore.BLUE + Startup.startup_text)
+                print(Fore.RESET)
+            elif current_color == "GREEN":
+                print(Fore.GREEN + Startup.startup_text)
+                print(Fore.RESET)
+            elif current_color == "WHITE":
+                print(Fore.WHITE + Startup.startup_text)
+                print(Fore.RESET)
+            elif current_color == "MAGENTA":
+                print(Fore.MAGENTA + Startup.startup_text)
+                print(Fore.RESET)
+
     current_dir = os.getcwd()
     # Continue until exit
     while True:
         # Print a starting point without '\n'
         if show_dir:
-            print(Fore.GREEN + current_dir, end="")
-        print(Fore.BLUE + "$ ", end="")
+            if not color_dict:
+                print(Fore.GREEN + current_dir, end="")
+            else:
+                if color_dict["DIR"]:
+                    current_color = color_dict["DIR"]
+                    if current_color == "MAGENTA":
+                        print(Fore.MAGENTA + current_dir, end="")
+                    elif current_color == "RED":
+                        print(Fore.RED + current_dir, end="")
+                    elif current_color == "BLUE":
+                        print(Fore.BLUE + current_dir, end="")
+                    elif current_color == "GREEN":
+                        print(Fore.GREEN + current_dir, end="")
+                    elif current_color == "WHITE":
+                        print(Fore.WHITE + current_dir, end="")
+
+        if not color_dict:
+            print(Fore.BLUE + "$ ", end="")
+        else:
+            if color_dict["SIGN"]:
+                current_color = color_dict["SIGN"]
+                if current_color == "BLUE":
+                    print(Fore.BLUE + "$ ", end="")
+                elif current_color == "RED":
+                    print(Fore.RED + "$ ", end="")
+                elif current_color == "GREEN":
+                    print(Fore.GREEN + "$ ", end="")
+                elif current_color == "MAGENTA":
+                    print(Fore.MAGENTA + "$ ", end="")
+                elif current_color == "WHITE":
+                    print(Fore.WHITE + "$ ", end="")
+
         print(Style.RESET_ALL, end="")
+
+        if color_dict:
+            if color_dict["INPUT"]:
+                current_color = color_dict["INPUT"]
+                if current_color == "BLUE":
+                    print(Fore.BLUE, end="")
+                elif current_color == "RED":
+                    print(Fore.RED, end="")
+                elif current_color == "GREEN":
+                    print(Fore.GREEN, end="")
+                elif current_color == "MAGENTA":
+                    print(Fore.MAGENTA, end="")
+                elif current_color == "WHITE":
+                    print(Fore.WHITE, end="")
+
         input_cmd = input()
         if input_cmd == "__exit__":  # exit case
             break
         elif input_cmd == "__help__":
+            print(Style.RESET_ALL, end="")
+
+            if color_dict:
+                if color_dict["HELP"]:
+                    current_color = color_dict["HELP"]
+                    if current_color == "BLUE":
+                        print(Fore.BLUE, end="")
+                    elif current_color == "RED":
+                        print(Fore.RED, end="")
+                    elif current_color == "GREEN":
+                        print(Fore.GREEN, end="")
+                    elif current_color == "MAGENTA":
+                        print(Fore.MAGENTA, end="")
+                    elif current_color == "WHITE":
+                        print(Fore.WHITE, end="")
+
             print(Startup.help_text)
         elif input_cmd == "view":
             files = view(current_dir)
@@ -80,8 +189,24 @@ def main():
             print(current_dir)
         elif input_cmd == "show_current":
             show_dir = True
+            config_dict["show_dir"] = True
+            try:
+                with open(json_file_path2, "w") as json_file:
+                    json.dump(config_dict, json_file, indent=4)
+            except FileNotFoundError:
+                print(f"JSON file not found at {json_file_path2}")
+            except Exception as e:
+                print(f"Error writing to JSON file: {e}")
         elif input_cmd == "hide_current":
             show_dir = False
+            config_dict["show_dir"] = False
+            try:
+                with open(json_file_path2, "w") as json_file:
+                    json.dump(config_dict, json_file, indent=4)
+            except FileNotFoundError:
+                print(f"JSON file not found at {json_file_path2}")
+            except Exception as e:
+                print(f"Error writing to JSON file: {e}")
         elif input_cmd.startswith("create_file "):
             file_name = input_cmd[12:]
             create_file(file_name)
